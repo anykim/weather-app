f847666940e0408c381d00c46ea00971f24c8ecd
Amy <amy.kim2332@gmail.com>
2023-08-22 16:02:09 -0700

    Get weather data from weather API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,57 @@
 'use client'
-import React, { useState, useEffect } from 'react'
-import { mtvWeather, vanWeather } from '@/utils/weather-api-helpers';
 
+import React, { useState, useEffect } from 'react'
+import { getMtvWeather, getVanWeather } from '@/utils/weather-api-helpers';
 
 export default function Home() {
+  const [mtvWeatherData, setMtvWeatherData] = useState<any>();
+  const [vanWeatherData, setVanWeatherData] = useState<any>();
+
+  const _getMtvWeaterData = async () => {
+    const mtvWeather = await getMtvWeather();
+    const mtvData = {
+      currentTemp: mtvWeather?.currentConditions.temp,
+      feelsLike: mtvWeather?.currentConditions.feelslike,
+      conditions: mtvWeather?.currentConditions.conditions,
+      humidity: mtvWeather?.currentConditions.humidity,
+      precipProb: mtvWeather?.currentConditions.precipProb,
+      sunrise: mtvWeather?.currentConditions.sunrise,
+      sunset: mtvWeather?.currentConditions.sunset,
+      uvIndex: mtvWeather?.currentConditions.uvindex,
+      windSpeed: mtvWeather?.currentConditions.windspeed,
+      windGust: mtvWeather?.currentConditions.temp,
+    }
+    setMtvWeatherData(mtvData);
+  }
+
+  const _getVanWeatherData = async () => {
+    const vanWeather = await getVanWeather();
+    const vanData = {
+      currentTemp: vanWeather?.currentConditions.temp,
+      feelsLike: vanWeather?.currentConditions.feelslike,
+      conditions: vanWeather?.currentConditions.conditions,
+      humidity: vanWeather?.currentConditions.humidity,
+      precipProb: vanWeather?.currentConditions.precipProb,
+      sunrise: vanWeather?.currentConditions.sunrise,
+      sunset: vanWeather?.currentConditions.sunset,
+      uvIndex: vanWeather?.currentConditions.uvindex,
+      windSpeed: vanWeather?.currentConditions.windspeed,
+      windGust: vanWeather?.currentConditions.temp,
+    };
+    setVanWeatherData(vanData);
+  }
 
   useEffect(() => {
-    mtvWeather()
-    vanWeather()
+    _getMtvWeaterData();
+    _getVanWeatherData()
   }, []);
-
-//   return (
-//     <main>    </main>
-//   )
+  
+  return (
+    <main>
+      <div>
+        <p>{vanWeatherData?.currentTemp}</p>
+        <p>{mtvWeatherData?.currentTemp}</p>
+      </div>
+    </main>
+  )
 }
--- src/utils/weather-api-helpers.ts
+++ src/utils/weather-api-helpers.ts
@@ -1,11 +1,13 @@
-export const mtvWeather = async () => {
+export const getMtvWeather = async () => {
     const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/mountain%20view?unitGroup=us&key=QY2DRNRWQTJAP2TGYS2ZZT8SR&contentType=json");
     const mtvWeatherData = await response.json();
     console.log(mtvWeatherData);
+    return mtvWeatherData
   };
 
-export const vanWeather = async () => {
+export const getVanWeather = async () => {
     const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/vancouver?unitGroup=us&key=QY2DRNRWQTJAP2TGYS2ZZT8SR&contentType=json");
     const vanWeatherData = await response.json();
     console.log(vanWeatherData);
+    return vanWeatherData
   };
