3c59277a519919bdc5d628c4771df5ffe413388f
Amy <amy.kim2332@gmail.com>
2023-08-28 12:04:28 -0700

    Converted temperature to celsius and updated README

--- src/app/WeatherCard.tsx
+++ src/app/WeatherCard.tsx
@@ -10,12 +10,12 @@ export const WeatherCard = ({ weatherData }: WeatherCardProps ) => {
                 <div className="flex flex-col flex-wrap items-center">
                     <p className="text-4xl font-semibold text-cyan-400 pt-14">{weatherData.city}</p>
                     <p className="text-xl text-cyan-200 pt-16 pb-5">Current Conditions:</p>
-                    <p className="text-2xl text-white">{weatherData.currentTemp} {'\u00b0'}F</p>
+                    <p className="text-2xl text-white">{Math.round((weatherData.currentTemp - 32) * 5/9)} {'\u00b0'}C</p>
                     <p className="text-2xl text-white">{weatherData.conditions}</p>
                 </div>
                 <div className="flex flex-row flex-wrap justify-center pt-14">
                     <p className="text-xl text-cyan-200">Feels like: </p>
-                    <p className="text-xl text-white pl-5">{weatherData.feelsLike} {'\u00b0'}F</p>
+                    <p className="text-xl text-white pl-5">{Math.round((weatherData.feelsLike - 32) * 5/9)} {'\u00b0'}C</p>
                 </div >
                 <div className="flex flex-row flex-wrap justify-center pt-20">
                     <p className="text-lg text-cyan-200">Precipitation %: </p>
